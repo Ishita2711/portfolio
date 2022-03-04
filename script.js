63498c7b849aacdfcd6ad5a6b57e1e2f68dc7224
Ishita Sharma <66726780+Ishita2711@users.noreply.github.com>
2022-03-04 11:55:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log("script running...")
 document.querySelector('.cross').style.display='none';
 document.querySelector('.hamp').addEventListener("click",()=>{
     document.querySelector('.sidebar').classList.toggle('sidebargo');
@@ -11,5 +10,18 @@ document.querySelector('.hamp').addEventListener("click",()=>{
         setTimeout(() =>{
             document.querySelector('.cross').style.display='inline'
     }, 300);
+}
+})
+document.querySelector('.cross').addEventListener("click",()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+    if(!document.querySelector('.sidebar').classList.contains('sidebargo')){
+        document.querySelector('.hamp').style.display='none'
+        document.querySelector('.cross').style.display='inline'
+    }
+    else{
+        document.querySelector('.cross').style.display='none'
+        setTimeout(() =>{
+        document.querySelector('.hamp').style.display='inline'   
+    }, 350);
 }
 })
